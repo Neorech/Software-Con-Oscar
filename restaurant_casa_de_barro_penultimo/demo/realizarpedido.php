dfb5f757ee09374c4c86a02536f1db7ea61f558c
oscar garay <oscar.garay-velasquez@talanlabs.com>
2017-02-08 23:05:42 +0100

    primer avance

--- restaurant_casa_de_barro_penultimo/demo/realizarpedido.php
+++ restaurant_casa_de_barro_penultimo/demo/realizarpedido.php
@@ -10,13 +10,13 @@
 	$idpersona=$_SESSION['persona'];
 	$idmesa=$_SESSION['mesa'];
 	if ($idpersona==null) {
-	echo '<div id="10668"><div id="alerta" >Seleccione una persona</div></div>';
-	exit;
+		echo '<div id="10668"><div id="alerta" >Seleccione una persona</div></div>';
+		exit;
 	}
 
 	if ($idmesa==null) {
-	echo '<div id="10668"><div id="alerta" >Seleccione una mesa</div></div>';
-	exit;
+		echo '<div id="10668"><div id="alerta" >Seleccione una mesa</div></div>';
+		exit;
 	}
 	
 	/*foreach ($_SESSION["arr"] as $i => $value){
@@ -32,10 +32,21 @@
 	}
 	
 
-	$rsmesa = mysql_query("SELECT mPago FROM mesa where mId='$idmesa'");
-	mysql_query("UPDATE mesa SET mEstado= '1', mPago = '1' where mId =".$idmesa." ;");
+	$rsmesa = mysql_query("SELECT
+							mPago
+						FROM 
+							mesa
+						WHERE mId='$idmesa'");
+
+	mysql_query("UPDATE
+					mesa
+				SET
+					mEstado= '1',
+					mPago = '1'
+				WHERE mId =".$idmesa." ;");
+
 	if ($rowmesa = mysql_fetch_row($rsmesa)) {
-	$idmesaestado = trim($rowmesa[0]);
+		$idmesaestado = trim($rowmesa[0]);
 	}
 	echo "mesa estado: ".$idmesaestado;
 
@@ -48,7 +59,7 @@
 									vMesa,
 									vEstado
 								FROM `venta`
-								where
+								WHERE
 									vMesa = '$idmesa'
 								order by vId
 								desc limit 1");
@@ -60,25 +71,54 @@
 		echo "mesa pedido mesa: ".$idpedidomesa;
 
 		foreach ($_SESSION["arr"] as $valor) {
+			//Actualiza Almacen 
+			//mysql_query("update mesa set mFecha='$fecha' where mId='$idmesa'");	
+			$resultalmacen = mysql_query("SELECT
+											idProductoAlmacen,
+											idProducto,
+											idAlmacen,
+											paCantidad
+										FROM
+											productoalmacen
+										WHERE idProducto='".$valor['id']."'");
+			//debemos verificar que hay stock
+			// para cada idProducto nos fijamos en almacen si la cantidad demandada - cantidad en almacen es mayor a cero
+			//si alguna falla, entonces hacemos un echo + exit;
+
+			if ($rowalmacen = mysql_fetch_array($resultalmacen)) { 
+				do {
+				//echo 'id Producto : '.$rowalmacen["idProducto"].'  '.$rowalmacen["idAlmacen"].'  '.$rowalmacen["paCantidad"].'<br/>';
+				mysql_query("UPDATE
+								almacen
+							SET
+								aCantidad = aCantidad - ".($rowalmacen["paCantidad"]*$valor['cantidad'])." where idAlmacen =".$rowalmacen["idAlmacen"]." ");
+				}
+				while ($rowalmacen = mysql_fetch_array($resultalmacen)); 
+			}
+
 			//echo 'id: '. $valor['id'].'id: '. $valor['cantidad'].'<br/>';
 
 			echo '//////////'.$idventatiempo=$valor['id'];
-			mysql_query("UPDATE venta SET vTimbre = 1, vFecha ='$fecha' WHERE vId=$idpedidomesa");
+			mysql_query("UPDATE
+							venta
+						SET
+							vTimbre = 1,
+							vFecha ='$fecha'
+						WHERE vId=$idpedidomesa");
 
 
-			mysql_query("INSERT INTO venta_detalle(vId,vd_idProducto, vd_Cantidad, vd_Costo) VALUES
-												('$idpedidomesa','".$valor['id']."','".$valor['cantidad']."','".$valor['costo']."')");
+			mysql_query("INSERT INTO
+							venta_detalle
+								(vId,
+								vd_idProducto,
+								vd_Cantidad,
+								vd_Costo)
+						VALUES ('$idpedidomesa',
+								'".$valor['id']."',
+								'".$valor['cantidad']."',
+								'".$valor['costo']."')");
 
-			//Actualiza Almacen 
-			//mysql_query("update mesa set mFecha='$fecha' where mId='$idmesa'");	
-			$resultalmacen = mysql_query("select idProductoAlmacen, idProducto, idAlmacen, paCantidad  from productoalmacen where idProducto='".$valor['id']."'"); 
-			if ($rowalmacen = mysql_fetch_array($resultalmacen)) { 
-				do { 
-		   		//echo 'id Producto : '.$rowalmacen["idProducto"].'  '.$rowalmacen["idAlmacen"].'  '.$rowalmacen["paCantidad"].'<br/>';
-				mysql_query("UPDATE almacen SET aCantidad = aCantidad - ".($rowalmacen["paCantidad"]*$valor['cantidad'])." where idAlmacen =".$rowalmacen["idAlmacen"]." ");
-				}
-				while ($rowalmacen = mysql_fetch_array($resultalmacen)); 
-		    }
+
 			//Actualiza Almacen 
 			//mysql_query("UPDATE venta SET vCantidadProducto = '20', vFecha= '16/09/1986' ,vIdusuario='1',vIdTiket='5'");
 		}
@@ -92,29 +132,72 @@
 	//insertar pedido extra
 	else {
 		//echo "persona" .$idpersona;
-		mysql_query("update mesa set mFecha='$fecha', mEstado='1' where mId='$idmesa'");
+		mysql_query("UPDATE
+						mesa
+					SET
+						mFecha='$fecha',
+						mEstado='1'
+					WHERE
+						mId='$idmesa'");
 
 
-		mysql_query("INSERT INTO venta (vCantidadProducto, vFecha, vIdpersona,vMesa,vObservacion,vTimbre) VALUES ('20','$fecha','$idpersona','$idmesa','$observacion','1')");
+		mysql_query("INSERT INTO
+						venta
+							(vCantidadProducto,
+							vFecha,
+							vIdpersona,
+							vMesa,
+							vObservacion,
+							vTimbre)
+					VALUES ('20',
+							'$fecha',
+							'$idpersona',
+							'$idmesa',
+							'$observacion',
+							'1')");
 
-		$rs = mysql_query("SELECT MAX(vId) AS id FROM venta");
+		$rs = mysql_query("SELECT
+								MAX(vId) AS id
+							FROM venta");
+
 		if ($row = mysql_fetch_row($rs)) {
-		$idultimo = trim($row[0]);
+			$idultimo = trim($row[0]);
 		}
 
 		//echo $idultimo;
 
 		foreach ($_SESSION["arr"] as $valor) {
 			//echo 'id: '. $valor['id'].'id: '. $valor['cantidad'].'<br/>';
-			mysql_query("INSERT INTO venta_detalle(vId,vd_idProducto, vd_Cantidad, vd_Costo) VALUES ('$idultimo','".$valor['id']."','".$valor['cantidad']."','".$valor['costo']."')");
+			mysql_query("INSERT INTO
+							venta_detalle
+								(vId,
+								vd_idProducto,
+								vd_Cantidad,
+								vd_Costo)
+						VALUES ('$idultimo',
+								'".$valor['id']."',
+								'".$valor['cantidad']."',
+								'".$valor['costo']."')");
 
 			//Actualiza Almacen 
 			//mysql_query("update mesa set mFecha='$fecha' where mId='$idmesa'");	
-			$resultalmacen = mysql_query("select idProductoAlmacen, idProducto, idAlmacen, paCantidad  from productoalmacen where idProducto='".$valor['id']."'"); 
+			$resultalmacen = mysql_query("SELECT
+											idProductoAlmacen,
+											idProducto,
+											idAlmacen,
+											paCantidad
+										FROM
+											productoalmacen
+										WHERE
+											idProducto='".$valor['id']."'");
+
 			if ($rowalmacen = mysql_fetch_array($resultalmacen)) { 
 				do { 
 		   			//echo 'id Producto : '.$rowalmacen["idProducto"].'  '.$rowalmacen["idAlmacen"].'  '.$rowalmacen["paCantidad"].'<br/>';
-					mysql_query("UPDATE almacen SET aCantidad = aCantidad - ".($rowalmacen["paCantidad"]*$valor['cantidad'])." where idAlmacen =".$rowalmacen["idAlmacen"]." ");
+					mysql_query("UPDATE
+									almacen
+								SET
+									aCantidad = aCantidad - ".($rowalmacen["paCantidad"]*$valor['cantidad'])." where idAlmacen =".$rowalmacen["idAlmacen"]." ");
 				}
 				while ($rowalmacen = mysql_fetch_array($resultalmacen)); 
 		    }
@@ -134,7 +217,12 @@
 	}
 
 
-	$rstop = mysql_query("SELECT vId FROM venta order by vId desc limit 1");
+	$rstop = mysql_query("SELECT
+							vId
+						FROM
+							venta
+						order by vId desc limit 1");
+
 	//mysql_query("UPDATE mesa SET mEstado= '0', mPago = '1' where mId =".$idmesa." ;");
 	if ($rowtop = mysql_fetch_row($rstop)) {
 		$topuno = trim($rowtop[0]);
